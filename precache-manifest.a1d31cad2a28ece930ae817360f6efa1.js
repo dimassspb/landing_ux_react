c987d1a588429c1ec75127ced6ab21fa41e0a9f6
Dmitrii Isakov <dimass82@mail.ru>
2022-11-28 21:32:48 +0300

    Updates

---
+++ precache-manifest.a1d31cad2a28ece930ae817360f6efa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72e8750b7c88d9b73934",
+    "url": "/landing_ux_react/static/css/main.02999820.chunk.css"
+  },
+  {
+    "revision": "72e8750b7c88d9b73934",
+    "url": "/landing_ux_react/static/js/main.72e8750b.chunk.js"
+  },
+  {
+    "revision": "85f011f1be934a162b9f",
+    "url": "/landing_ux_react/static/css/1.32244e9d.chunk.css"
+  },
+  {
+    "revision": "85f011f1be934a162b9f",
+    "url": "/landing_ux_react/static/js/1.85f011f1.chunk.js"
+  },
+  {
+    "revision": "97c2d3eab94bcd5dfee5",
+    "url": "/landing_ux_react/static/js/runtime~main.97c2d3ea.js"
+  },
+  {
+    "revision": "830b1d9122f0fb8cf59d1f3b4300a8ac",
+    "url": "/landing_ux_react/index.html"
+  }
+];
